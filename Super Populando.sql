1b3110c4623867af65b890d1ba81d945378b1504
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2023-06-17 18:14:31 -0300

    Super Populando sql pt 1

--- Super Populando.sql
+++ Super Populando.sql
@@ -1,1 +1,21 @@
 /*Super populando*/
+insert into
+cliente values
+(null, 'Marcio Dev Senior', 'modelagem@fadergs.com', '1234', '11111111111', '551140028922', 'Java nao e bom', 'php e melhor', 'prefiro laravel', '99999999','10000.00'),
+(null, 'Flavio Churrasqueiro', 'modelagemeprogramacao@fadergs.com', '1234', '11111111112', '551140028922', 'paulada sem perguntar', 'do while pergunta depois','descubram biblioteca sozinhos', '99999998', '15000.00'),
+(null, 'Deivid Sorveteiro', 'programacao@fadergs.com', '1234', '11111111113', '551140028922', 'vou ligar', 'o ar no', 'inverno kkk', '99999997', '5000.00'),
+(null, 'Adalto Palestrante', 'extensao@fadergs.com', '1234', '11111111114', '551140028922', 'venham todos', 'para aula importante', 'e so palestra kkkkk', '99999996', '20000.00'), 
+(null, 'Robson Fazendo Forum', 'forunista@fadergs.com', '1234', '11111111115', '551140028922', 'eu fiz', 'o forum', 'em modelagem', '99999995', '1200.00'),
+(null, 'Menina Comunista do Verdinho', 'comunismo@fadergs.com', '1234', '11111111116', '551140028922', 'nao assisto aula', 'mas vou atrapalhar', 'com palestra chata', '99999994', '10000000.00'),
+(null, 'Luan Bola Oito', 'seguranca@fadergs.com', '1234', '11111111117', '551140028922', 'os numeros refletem', 'na minha cabeca', 'enquanto dou aula', '99999993','20000.00'),
+(null, 'Denicol Dr Fran', 'drfrandenicol@fadergs.com', '1234', '11111111118', '551140028922', 'tcp ip', 'descobrir mascara', 'ipv4', '99999992', '3000.00'),
+(null, 'garoto de oculos experiente', 'naoseionome@fadergs.com', '1234', '11111111119', '551140028922', 'ja sei tudo', 'so quero', 'o diploma', '99999991', '100000.00'),
+(null, 'Andressa Traidora da Patria', 'turnomanha@fadergs.com', '1234', '11111111120', '551140028922', 'minha vida e', 'andar pelo centro', 'colar do marcus', '99999919', '500.00');
+
+insert into
+funcionario values
+(null, 'Marcus Vinicius', 'capitaoexercito@fadergs.com', '1234', '11111111121', '551140028922', 'vou servir', 'programacao e segundo', 'lugar', '99999920', '1500.00'),
+(null, 'Matheus Correa', 'melhordev@fadergs.com', '1234', '11111111122', '551140028922', 'sou o cara', 'do back end', 'ninguem me ganha', '99999921', '10000.00'),
+(null, 'Marcelo Dono da Quebrada', 'bagulhodoido@fadergs.com', '1234', '11111111123', '551140028922', 'ninguem me segura', 'ladrao', 'vou ser pai', '99999922', '650.00'),
+(null, 'Yasmin Gata da quebrada', 'qualvaisermenor@escola.com', '1234', '11111111124', '551140028922', 'sou uma gata', 'linda e gostosa', 'vou ser mãe', '99999923', '0.00'),
+(null, 'Carinha do elevador', 'destrancoelevador@fadergs.com', '1234', '11111111125', '551140028922', 'eu escolho', 'onde o cliente', 'entra', '99999924','1000.00');
